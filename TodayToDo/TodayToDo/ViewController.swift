3fe6b549c1f58e333d9e7911a872af0484debb59
AlexCheGH <Iphone3gs!>
2022-01-08 12:46:52 +0300

    Initial project creation

---
+++ TodayToDo/TodayToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodayToDo
+//
+//  Created by Alex Chekushkin on 1/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
