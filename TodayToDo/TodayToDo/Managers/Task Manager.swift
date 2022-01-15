76abdd04a9102a8de4dcd466b1f257be4f0d2c25
AlexCheGH <Iphone3gs!>
2022-01-16 00:06:21 +0300

    done
    added UI task creation functionality
    added commentaries for extensions

--- TodayToDo/TodayToDo/Managers/Task Manager.swift
+++ TodayToDo/TodayToDo/Managers/Task Manager.swift
@@ -20,12 +20,12 @@ struct TaskManager {
         self.loadTasks()
     }
     
-   private mutating func loadTasks() {
+    private mutating func loadTasks() {
         do { self.tasks = try context.fetch(Task.fetchRequest() ) as [Task] }
         catch { print("An error occured when trying to fetch tasks.") }
     }
     
-   
+    
     private mutating func saveEntry() {
         do {
             try context.save()
@@ -33,11 +33,16 @@ struct TaskManager {
         }
         catch { print("An error occured when trying to save the task.") }
     }
-        
+    
     /// Gets a task to populate UIView/UICell
     func getTask(preciseTaskDate: String) -> Task {
-        userTasks.filter{ $0.tasksDate as! String == preciseTaskDate }.first! //should always exist
-    }
+        guard let task = userTasks.filter({ $0.tasksDate as! String == preciseTaskDate }).first else {
+            
+            let task = Task(context: context)
+            return task }
+        
+        return task
+    } //should always exist
     
     /// Function decides whether to edit or create new task. If "preciseDate" is nil -> creates new task
     mutating func createNewTask(title: String, description: String?, taskIsDone: Bool = false, preciseDate: String?) {
@@ -55,24 +60,24 @@ struct TaskManager {
     
     // Creates a fresh task
     private mutating func addNewEntry(title: String, description: String?, taskIsDone: Bool = false, preciseDate: String) {
-         let task = Task(context: context)
-         task.taskTitle = title as NSObject
-         task.taskDescription = (description ?? "") as NSObject
-         task.taskIsDone = taskIsDone as NSObject
-         task.tasksDate = preciseDate as NSObject
-         
-         saveEntry()
-     }
+        let task = Task(context: context)
+        task.taskTitle = title as NSObject
+        task.taskDescription = (description ?? "") as NSObject
+        task.taskIsDone = taskIsDone as NSObject
+        task.tasksDate = preciseDate as NSObject
+        
+        saveEntry()
+    }
     
     // Edits an existing task
     private mutating func editEntry(title: String, description: String?, taskIsDone: Bool = false, preciseDate: String) {
-            let task = getTask(preciseTaskDate: preciseDate)
-            task.tasksDate = preciseDate as NSObject?
-            task.taskIsDone = taskIsDone as NSObject
-            task.taskDescription = (description ?? "") as NSObject
-            task.taskTitle = title as NSObject?
-            
-            saveEntry()
+        let task = getTask(preciseTaskDate: preciseDate)
+        task.tasksDate = preciseDate as NSObject?
+        task.taskIsDone = taskIsDone as NSObject
+        task.taskDescription = (description ?? "") as NSObject
+        task.taskTitle = title as NSObject?
+        
+        validateTask(task: task) ? saveEntry() : Void()
     }
     
     /// Removes task for the specific date
@@ -88,4 +93,22 @@ struct TaskManager {
             loadTasks()
         }
     }
+    
+    // Validates whether a task needs to be created or not. If task title is empty it wont be created. If a task lack title, but has a description it'll be named as the description first symbols
+    private func validateTask(task: Task) -> Bool {
+        let title = task.taskTitle as! String
+        let description = task.taskDescription as! String
+        
+        let titleIsEmpty = title.filter{ $0 != " " }.isEmpty
+        let descriptionIsEmpty = description.filter{ $0 != " " }.isEmpty
+        
+        //if title is empty, but description is not, the tittle will be saved as the initial 10 characters of the description
+        if titleIsEmpty && !descriptionIsEmpty {
+            let text = String(description.prefix(10) + "...")
+            task.taskTitle = text as NSObject
+            return true
+        }
+        return false
+    }
+    
 }
