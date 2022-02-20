91e4f0a9dd4207ba6b879538577e1cff5e55f663
AlexCheGH <Iphone3gs!>
2022-02-20 22:45:19 +0100

    fixed table view cell on main VC

--- TodayToDo/TodayToDo/Views/ToDoTableViewCell/ToDoTableViewCell.swift
+++ TodayToDo/TodayToDo/Views/ToDoTableViewCell/ToDoTableViewCell.swift
@@ -17,8 +17,8 @@ class ToDoTableViewCell: UITableViewCell {
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var taskNameLabel: UILabel!
     
-    private let statusDoneString = "✔️"
-    private let statusNotDoneString = "🔲"
+    private let checkmark = "✔️"
+    private let emptyBox = "🔲"
     private var taskIsDone = false
     
     var delegate: ToDoCellProtocol?
@@ -68,17 +68,28 @@ class ToDoTableViewCell: UITableViewCell {
     }
     
     private func changeStatusLabel(isDone: Bool) {
+        guard let text = taskNameLabel.text else { return }
         switch isDone {
         case true:
-            statusLabel.text = statusDoneString
-            markCellDone()
+            markCellDone(text: text)
         case false:
-            statusLabel.text = statusNotDoneString
+            markCellUndone(text: text)
         }
     }
     
-    private func markCellDone() {
-        guard let text = taskNameLabel.text else { return }
+    private func markCellUndone(text: String) {
+        statusLabel.text = emptyBox
+        
+        let attributedString = NSMutableAttributedString(string: text)
+        attributedString.removeAttribute(NSAttributedString.Key.strikethroughStyle, range: NSMakeRange(0, attributedString.length))
+        taskNameLabel.attributedText = attributedString
+        
+        self.backgroundColor = .white
+    }
+    
+    private func markCellDone(text: String) {
+        statusLabel.text = checkmark
+        
         let attributedString = NSAttributedString(string: text, attributes: [NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single.rawValue])
         taskNameLabel.attributedText = attributedString
         
